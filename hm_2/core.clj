846760b2ccc9bec54c2dc70e5a9b2a2244d0a5cb
alchernov2102 <alch2102@proton.me>
2024-05-30 15:59:22 +0200

    Add merge-with

--- hm_2/core.clj
+++ hm_2/core.clj
@@ -72,3 +72,32 @@
 (= ["HELLO" 5] ((my-juxt #(.toUpperCase %) count) "hello"))
 (= [2 6 4] ((my-juxt :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
 
+
+
+;; elegant solution - not mine, added for testing and debugging
+(defn my-merge-with [f & maps]
+  (->> maps 
+    ;; add each key-value pair to the list of vectors  
+    (apply concat) 
+    ;; returns a map {:a [[:a 1] [:a 2]]}   
+    (group-by key) 
+    ;; get key and reduce a function for every value   
+    (map (fn [[k vs]] [k (reduce f (map val vs))])) 
+    ;; return map instead of list   
+    (into {})
+    ))
+
+(comment
+  "Returned by (group-by key maps)"
+  {:a [[:a 1] [:a 3]], :b [[:b 2]]})
+
+(my-merge-with + {:a 1 :b 2} {:a 3})
+
+;; Tests
+(= (my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+   {:a 4, :b 6, :c 20})
+(= (my-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15})
+   {1 7, 2 10, 3 15})
+(= (my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+   {:a [3 4 5], :b [6 7], :c [8 9]})
+
